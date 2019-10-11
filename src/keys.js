6d431c2fda471bde39609c08c1bf1c1622b18b90
root <root@tania-concejal.us-central1-a.c.tania-concejal.internal>
2019-10-11 01:49:32 +0000

    normal

--- src/keys.js
+++ src/keys.js
@@ -2,8 +2,8 @@ module.exports={
 
     database:{
         host:'localhost',
-        user: 'root',
-        password:'',
+        user: 'jacopete',
+        password:'jacopete014014014',
         database:'programa'
     }
 
